2c317bdff4807506a7f78a97db81535290eec856
NtwaliHeritier <heritierntwali@gmail.com>
2021-03-12 04:39:51 +0200

    add the dropdown to the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 // import './App.css';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
-import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <div className="container mt-5">
         <BooksList />
         <BooksForm />
